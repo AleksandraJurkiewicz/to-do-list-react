5e91aa745b0f245fb09890a7dbcc026d53945f04
Aleksandra Jurkiewicz <aleksandra.jurkiewicz2003@gmail.com>
2025-03-19 13:08:35 +0100

    unnecessary files deleted

--- src/Container/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Wrapper } from "./styled";
-
-const Container = (props) => (
-    <Wrapper>
-        {props.children}
-    </Wrapper>
-);
-
-export default Container;
--- src/styled.js
+++ src/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Container = styled.div`
     max-width: 900px;
     min-width: 300px;
     margin: 0 auto;
