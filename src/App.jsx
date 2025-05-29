54dbacbf1ce267b90a194e422f33579a1915476d
Aleksandra Jurkiewicz <aleksandra.jurkiewicz2003@gmail.com>
2025-05-29 16:50:59 +0200

    redux-router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-import Tasks from "./features/tasks/Tasks";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import Author from "./features/author/Author";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { toAuthor, toTask, toTasks } from "./routes";
+import Navigation from "./Navigation";
 
-const App = () => <Tasks />
-
-export default App;
+export default () => (
+    <HashRouter>
+        <Navigation />
+        <Switch>
+            <Route path={toTask()}>
+                <TaskPage />
+            </Route>
+            <Route path={toTasks()}>
+                <TasksPage />
+            </Route>
+            <Route path={toAuthor()}>
+                <Author />
+            </Route>
+            <Route>
+                <Redirect to={toTasks()} />
+            </Route>
+        </Switch>
+    </HashRouter>
+);
