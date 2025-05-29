f7fc5df755372da236207cab1e1df40d01e0f747
Aleksandra Jurkiewicz <aleksandra.jurkiewicz2003@gmail.com>
2025-05-29 17:25:56 +0200

    redux-saga added

---
+++ src/features/tasks/tasksSaga.jsx
@@ -0,0 +1,25 @@
+import { takeEvery, takeLatest, call, put, delay, select } from "redux-saga/effects";
+import { fetchExampleTasksSuccess, fetchExampleTasks, selectTasks, fetchExampleTasksError } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+    try {
+        yield delay(1000);
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(fetchExampleTasksSuccess(exampleTasks));
+    } catch (error) {
+        yield put(fetchExampleTasksError());
+        yield call(alert, "Coś poszło nie tak :C");
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
