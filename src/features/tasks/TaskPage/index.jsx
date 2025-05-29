d6e96810e3064c8114f9bd4d3fdcf650b395daa5
Aleksandra Jurkiewicz <aleksandra.jurkiewicz2003@gmail.com>
2025-05-29 17:37:43 +0200

    task page added

---
+++ src/features/tasks/TaskPage/index.jsx
@@ -0,0 +1,27 @@
+import Header from "../../../common/Header";
+import Section from "../../../common/Section";
+import Container from "../../../common/Container";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectTaskById } from "../tasksSlice";
+
+function TaskPage() {
+    const { id } = useParams();
+    const task = useSelector((state) => selectTaskById(state, id));
+
+    return (
+        <Container>
+            <Header title="Szczegóły zadania" />
+            <Section
+                title={task ? task.content : "Nie ma takiego zadania :C"}
+                sectionContent={!!task && (
+                    <>
+                        <strong>Ukończone:</strong> {task.done ? "Tak" : "Nie"}
+                    </>
+                )}
+            />
+        </Container>
+    )
+}
+
+export default TaskPage;
